da891cfb0e9e8a956cc7dab1d51ea83e485b2e6f
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2022-01-20 23:10:14 -0800

    Added deploy script, ERC721BridgableUserMintingTest.sol, and fixed bugs in ERC721A

---
+++ contracts/ERC721BridgableUserMintingTest.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./tokens/ERC721BridgableUserMinting.sol";
+
+/**
+ * @dev ERC-721 Contract with support for Xenum Bridge, 
+ * and allows contract owner to give users permission to mint their own items 
+ */
+contract ERC721BridgableUserMintingTest is ERC721BridgableUserMinting {
+   constructor() initializer {
+      __ERC721BridgableUserMinting_init("Test", "TST", 5000);
+   }
+
+	function mint(address _recipient, uint256 _amount) external {
+		_safeMint(_recipient, _amount, "");
+	}
+}
